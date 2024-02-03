e1dd8edcc72d4a99dcc713686b37d695a29d0acf
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-03 21:16:35 +0600

    META DATA

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter/Counter";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
@@ -6,6 +7,12 @@ const HomePage = () => {
     <div>
       This is home page
       <Counter />
+      <Link href="/about">
+        <button className="bg-blue-500 px-2 py-1 rounded my-2">About</button>
+      </Link>
+      <Link href="/contact">
+        <button className="bg-blue-500 px-2 py-1 rounded my-2">Contact</button>
+      </Link>
     </div>
   );
 };
